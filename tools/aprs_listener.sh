54281320b03771bc558cdda389f7dc533b0e7816
bakStaaJ Designs <39580347+bakstaaj@users.noreply.github.com>
2026-08-02 20:09:42 -0600

    Correct RTL APRS tuning offset

--- tools/aprs_listener.sh
+++ tools/aprs_listener.sh
@@ -8,7 +8,7 @@ audio_capture_enabled="${APRS_AUDIO_CAPTURE:-0}"
 audio_capture_seconds="${APRS_AUDIO_CAPTURE_SECONDS:-60}"
 mkdir -p "${runtime_dir}"
 if [[ "${audio_capture_enabled}" == "1" ]]; then
-  exec bash -c 'rtl_fm -d 00000144 -f 144390000 -M fm -s 240000 -r 48000 -g "$3" - 2>>"$1/rtl.log" | python3 "$4" --output "$1/audio-ring.wav" --seconds "$5" --sample-rate 48000 | stdbuf -oL direwolf -q h -r 48000 -c "$2" - 2>&1 | stdbuf -oL tee -a "$1/packets.log"' _ "${runtime_dir}" "${project_dir}/config/direwolf.aprs-rx.example.conf" "${rtl_gain_db}" "${project_dir}/tools/aprs_audio_capture.py" "${audio_capture_seconds}"
+  exec bash -c 'rtl_fm -d 00000144 -f 144390000 -M fm -E offset -s 240000 -r 48000 -g "$3" - 2>>"$1/rtl.log" | python3 "$4" --output "$1/audio-ring.wav" --seconds "$5" --sample-rate 48000 | stdbuf -oL direwolf -q h -r 48000 -c "$2" - 2>&1 | stdbuf -oL tee -a "$1/packets.log"' _ "${runtime_dir}" "${project_dir}/config/direwolf.aprs-rx.example.conf" "${rtl_gain_db}" "${project_dir}/tools/aprs_audio_capture.py" "${audio_capture_seconds}"
 else
-  exec bash -c 'rtl_fm -d 00000144 -f 144390000 -M fm -s 240000 -r 48000 -g "$3" - 2>>"$1/rtl.log" | stdbuf -oL direwolf -q h -r 48000 -c "$2" - 2>&1 | stdbuf -oL tee -a "$1/packets.log"' _ "${runtime_dir}" "${project_dir}/config/direwolf.aprs-rx.example.conf" "${rtl_gain_db}"
+  exec bash -c 'rtl_fm -d 00000144 -f 144390000 -M fm -E offset -s 240000 -r 48000 -g "$3" - 2>>"$1/rtl.log" | stdbuf -oL direwolf -q h -r 48000 -c "$2" - 2>&1 | stdbuf -oL tee -a "$1/packets.log"' _ "${runtime_dir}" "${project_dir}/config/direwolf.aprs-rx.example.conf" "${rtl_gain_db}"
 fi
